083f5a8a9745648c47e72841838090f6ad8fa8ad
Gabriel Wiggert <gabrielwiggert@gmail.com>
2022-04-03 00:33:10 -0300

    footer implemented

--- src/componentes/Footer.js
+++ src/componentes/Footer.js
@@ -1,5 +1,29 @@
+import styled from "styled-components";
+
 export default function Footer() {
     return(
-        <h1>Footer!</h1>
+        <Bottom>
+            <h1>Habitos</h1>
+            <h1>Hoje</h1>
+            <h1>Histórico</h1>
+        </Bottom>
     );
 }
+
+const Bottom = styled.div`
+    position: fixed;
+    bottom: 0px;
+    background-color: white;
+    height: 70px;
+    width: 100vw;
+
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 31px;
+    padding-right: 31px;
+
+    h1 {
+        color: #52B6FF;
+    }
+`;
